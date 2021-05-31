6829ef08647bdef4602e90e955605574ba1f4756
Drashti-711 <76907602+Drashti-711@users.noreply.github.com>
2021-05-31 23:33:26 +0530

    Day 4

---
+++ Day 4 Task/Book Store/contact.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Book Store Template</title>
+<link href="http://fonts.googleapis.com/css?family=Arvo" rel="stylesheet" type="text/css" />
+<link href="http://fonts.googleapis.com/css?family=Coda:400,800" rel="stylesheet" type="text/css" />
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+<div id="menu-wrapper">
+	<div id="menu">
+		<ul>
+			<li class="current_page_item"><a href="index.html">Homepage</a></li>
+			<li><a href="photos.html">Photos</a></li>
+			<li><a href="contact.php">Contact</a></li>
+		</ul>
+	</div>
+	<!-- end #menu --> 
+</div>
+<div id="header-wrapper">
+	<div id="header">
+		<div id="logo">
+		</div>
+	</div>
+</div>
+<div id="wrapper"> 
+	<!-- end #header -->
+	<div id="page-bgtop"></div>
+	<div id="page">
+		<h1 align="center">Contact Us</h1>
+	<div class="container">
+		<form action="" method="POST">
+			<div class="form-group">
+				<div class="col-lg-8">
+					<label for="name">Name : </label><br>
+					<input type="name" name="name" size="30" required/>
+				</div>
+			</div>
+			<br>
+			<div class="form-group">	
+				<div class="col-lg-8">
+					<label for="Email">Email :  </label><br>
+					<input type="Email" name="email" size="30" required/>
+				</div>
+			</div>
+			<br>
+			<div class="form-group">
+				<div class="col-lg-8">
+					<label for="number">Phone : </label><br>
+					<input type="txt" name="number" size="29" required/>
+				</div>
+			</div>
+			<br>
+			<div class="form-group">
+				<div class="col-lg-8">
+					<label for="Message">Message : </label><br>
+					<textarea id="subject" name="subject" placeholder="Write something.." style="height:100px ;width:500px ; " ></textarea>
+				</div>
+			</div>
+			<br>
+			<button class="button">Submit</button>
+		<!--	<div class="form-group">
+				//other method
+					<input class="button" type="submit" name="submit">
+			<!--</div>-->
+		</form>
+	</div>	
+		
+<div style="clear: both;">&nbsp;</div>
+</div>
+	
+<div id="page-bgbtm"></div>
+	<!-- end #page --> 
+</div>
+<div id="footer">
+	<p>Template managed by drashti</a></p>
+</div>
+<!-- end #footer -->
+</body>
+</html>
